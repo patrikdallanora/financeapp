fcc25e8ef0ee9900931557b718e2fc961a42c526
Patrik Dalla Nora <patrik@ibr.local>
2026-06-09 08:36:15 -0300

    teste v2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'financeapp-v2'
+const CACHE_NAME = 'financeapp-v3'
 
 const ASSETS = [
   '/',
--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
-      manifest: {
-        name: 'FinanceApp',
-        short_name: 'Finance',
-        description: 'Controle financeiro pessoal offline',
-        theme_color: '#000000',
-        background_color: '#000000',
-        display: 'standalone',
-        start_url: '/',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: '/icons/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icons/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()]
 })
